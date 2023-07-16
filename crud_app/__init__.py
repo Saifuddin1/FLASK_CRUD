1cdd84ee1f9063d487a64977d83ddbedf095f494
mohammad saifuddin <Mohammad.Saifuddin_ext@ltts.com>
2023-07-16 17:19:11 +0530

    add all files

--- crud_app/__init__.py
+++ crud_app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import secrets
 from crud_app.routes import crud_bp
+from crud_app.database import init_db
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = secrets.token_hex(16)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:saif123@saifuddin"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:saif123@localhost/saifuddin"
     app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
     app.register_blueprint(crud_bp, url_prefix="/")
+    init_db(app)
     return app
--- crud_app/routes.py
+++ crud_app/routes.py
@@ -1,9 +1,48 @@
-from flask import Blueprint
-
+from flask import Blueprint, render_template, request, redirect, url_for
+from crud_app.models import User
+from crud_app.database import db
 
 crud_bp = Blueprint("crud_bp", __name__)
 
 
-@crud_bp.route("/")
-def user_creation():
-    return "This is User Creation page"
+@crud_bp.route("/", methods=['GET', 'POST'])
+def index():
+    all_users = User.query.paginate(
+        page=request.args.get('page', 1, type=int), per_page=5, error_out=False)
+    if request.method == 'POST':
+        first_name = request.form.get("fname")
+        last_name = request.form.get("lname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('crud_bp.index'))
+    return render_template("index.html", all_users=all_users)
+
+
+@crud_bp.route("/update/<int:user_id>", methods=["GET", "POST"])
+def update(user_id):
+    user_to_update = User.query.get_or_404(user_id)
+    if request.method == 'POST':
+        user_to_update.first_name = request.form.get("fname")
+        user_to_update.last_name = request.form.get("lname")
+        user_to_update.email = request.form.get("email")
+        user_to_update.password = request.form.get("password")
+        db.session.commit()
+        return redirect(url_for('crud_bp.index'))
+
+    return render_template('update.html', user=user_to_update)
+
+
+@crud_bp.route("/delete/<int:user_id>", methods=["GET", "POST"])
+def delete(user_id):
+    user_to_delete = User.query.get_or_404(user_id)
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect(url_for('crud_bp.index'))
